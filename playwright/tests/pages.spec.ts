1f970567515de69c3dc23564d28af08b636f2a2c
a-rodriguez-c <ja.rodriguezc12@uniandes.edu.co>
2023-11-12 16:49:54 -0500

    gitignore

--- playwright/tests/pages.spec.ts
+++ playwright/tests/pages.spec.ts
@@ -11,6 +11,8 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('creación de pagina sin publicar', async ({ page }) => {
+  const newPageTitle = `test-${faker.word.noun()}`;
+
   await test.step('When: El usuario hace clic en "Pages"', async () => {
     await page.getByRole('link', { name: 'Pages' }).click();
   });
@@ -21,8 +23,7 @@ test('creación de pagina sin publicar', async ({ page }) => {
 
   await test.step('And: El usuario crea el título de la nueva pagina', async () => {
     await page.getByPlaceholder('Page title').click();
-    await page.getByPlaceholder('Page title').fill('Nueva Pagina Test');
-    await page.getByRole('paragraph').click();
+    await page.getByPlaceholder('Page title').fill(newPageTitle);
   });
 
   await test.step('And: El usuario publica la nueva pagina', async () => {
@@ -34,10 +35,9 @@ test('creación de pagina sin publicar', async ({ page }) => {
   });
 
   await test.step('Then: La nueva pagina se muestra en la lista de paginas creadas', async () => {
-    expect(await page.title()).toBe('Nueva Pagina Test - grupo 25');
+    expect(await page.title()).toBe(`${newPageTitle} - grupo 25`);
   });
 });
-
 
 test('creación de pagina publicada', async ({ page }) => {
   const newPageTitle = `test-${faker.word.noun()}`;
