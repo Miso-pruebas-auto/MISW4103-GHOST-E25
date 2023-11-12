f45c616c032c470f0ea36c5b5fdfe91f8a36e1a0
camilo lesmes <c.lesmesl@uniandes.edu.co>
2023-11-12 02:59:38 -0500

    implementado patron Given-When-Then en post

--- playwright/tests/post.spec.ts
+++ playwright/tests/post.spec.ts
@@ -1,39 +1,65 @@
 import { test, expect } from '@playwright/test';
 import { loginSessionAdmin } from '../utils/login_sesion_admin';
 
-test.describe('sección posts', () => {
+test.describe('Posts creación', () => {
 
   test.beforeEach(async ({ page }) => {    
+    // Given: El usuario ha iniciado sesión
     await loginSessionAdmin(page);
-    console.log("Usuario logeado con éxito ");
   });
 
-  test('Creación de un nuevo post', async ({ page }) => {
-    await page.getByRole('link', { name: 'New post' }).click();
-    await page.getByPlaceholder('Post title').click();
+  test('Crear un nuevo post con solo título y descripción', async ({ page }) => {
+    // When: El usuario crea un nuevo post
     const titulo_post = 'Titulo de prueba';
     const contenido = 'Contenido de prueba';
-    await page.getByPlaceholder('Post title').fill(titulo_post);
-    await page.getByPlaceholder('Post title').press('Tab');
-    await page.getByRole('textbox').nth(1).fill(contenido);
-    await page.getByRole('button', { name: 'Publish' }).click();
-    await page.getByRole('button', { name: 'Continue, final review →' }).click();
-    await page.waitForTimeout(1000);
+    
+    // And: El usuario hace clic en "New post"
+    await page.getByRole('link', { name: 'New post' }).click(); 
+    
+    // And: El usuario hace clic en el campo de título del post
+    await page.getByPlaceholder('Post title').click(); 
+    
+    // And: El usuario llena el título del post
+    await page.getByPlaceholder('Post title').fill(titulo_post); 
+    
+    // And: El usuario presiona Tab
+    await page.getByPlaceholder('Post title').press('Tab'); 
+    
+    // And: El usuario llena el contenido del post
+    await page.getByRole('textbox').nth(1).fill(contenido); 
+    
+    // And: El usuario hace clic en "Publish"
+    await page.getByRole('button', { name: 'Publish' }).click(); 
+    
+    // And: El usuario hace clic en "Continue, final review →"
+    await page.getByRole('button', { name: 'Continue, final review →' }).click(); 
+    
+    // And: Se espera un segundo
+    await page.waitForTimeout(1000); 
     const buttonSelector = `//button[contains(., 'Publish post, right now')]`;
-    await page.locator(buttonSelector).click();
-  
+    
+    // And: El usuario hace clic en el botón final de publicación
+    await page.locator(buttonSelector).click(); 
+    
+    // And: Se espera un segundo
+    await page.waitForTimeout(1000);
+    const dato = `${titulo_post} ${contenido} •andes`
+    
+    // And: El usuario hace clic en el enlace del post creado
     const page1Promise = page.waitForEvent('popup');
-    await page.getByRole('link', { name: `${titulo_post} ${contenido} grupo 25 •andes` }).click();
+    await page.getByRole('link', { name: dato }).click();
     const page1 = await page1Promise;
     await page1.locator('h1').click();
     
-    expect(await page1.locator('h1').innerHTML()).toBe(titulo_post);
-    console.log('El titulo es igual');
+    // And: El usuario hace clic en el título del post
+    await page1.locator('h1').click(); 
+    
+    // Then: Se verifica que el título y contenido son correctos
+    expect(await page1.locator('h1').innerText()).toBe(titulo_post); 
     const mainElement = await page1.locator('main');
     const contenidoActual = await mainElement.innerText();
-    expect(contenidoActual).toMatch(contenido);
-    console.log('El contenido es igual');
+    expect(contenidoActual).toMatch(contenido); 
+  
   });
 
 });
-
