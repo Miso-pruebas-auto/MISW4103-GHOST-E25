b383d424d4f56c67703ffa70184af12c60e7054d
a-rodriguez-c <ja.rodriguezc12@uniandes.edu.co>
2023-11-12 15:27:31 -0500

    wip: solving envs

--- playwright/tests/pages.spec.ts
+++ playwright/tests/pages.spec.ts
@@ -1,14 +1,12 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { loginSessionAdmin } from '../utils/login_sesion_admin';
-import { createUserAdmin } from '../utils/create_user_admin';
 
 test.describe.configure({ mode: 'serial' });
 
-let page: Page;
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  await loginSessionAdmin(page);
+test.beforeEach(async ({ page }) => {      
+  await test.step('Given: El usuario ha iniciado sesión', async () => {
+      await loginSessionAdmin(page);
+  });
 });
 
 test('creación de pagina sin publicar', async ({ page }) => {
