cd2a9aabec18f81fd018d09f6e9c9fc923258a4d
a-rodriguez-c <ja.rodriguezc12@uniandes.edu.co>
2023-11-11 21:21:13 -0500

    wip: playwright initial configuration

--- playwright/tests/master_test_create.spec.ts
+++ playwright/tests/master_test_create.spec.ts
@@ -1,15 +1,11 @@
 import { test } from '@playwright/test';
-import { loginSesionAdmin } from './1. login_sesion_admin.spec';
-// import { createUserAdminTest } from './2. create_user_admin.spec';
-// import { createPostTest } from './3. create_post.spec';
 
 test.describe('create post complete', () => {
     let loggedIn = false;
   
     test.beforeEach(async ({ page }) => {
       if (!loggedIn) {
-        await loginSesionAdmin(page);
-        loggedIn = true;
+        console.log();
       }
     });
   
