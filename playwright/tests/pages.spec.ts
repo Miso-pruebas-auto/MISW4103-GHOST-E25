988b347f8fc018254058a9d98e08e5ac08488ada
a-rodriguez-c <ja.rodriguezc12@uniandes.edu.co>
2023-11-12 16:35:17 -0500

    test: public page

--- playwright/tests/pages.spec.ts
+++ playwright/tests/pages.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { loginSessionAdmin } from '../utils/login_sesion_admin';
+import { faker } from '@faker-js/faker';
 
 test.describe.configure({ mode: 'serial' });
 
@@ -21,6 +22,7 @@ test('creación de pagina sin publicar', async ({ page }) => {
   await test.step('And: El usuario crea el título de la nueva pagina', async () => {
     await page.getByPlaceholder('Page title').click();
     await page.getByPlaceholder('Page title').fill('Nueva Pagina Test');
+    await page.getByRole('paragraph').click();
   });
 
   await test.step('And: El usuario publica la nueva pagina', async () => {
@@ -38,6 +40,8 @@ test('creación de pagina sin publicar', async ({ page }) => {
 
 
 test('creación de pagina publicada', async ({ page }) => {
+  const newPageTitle = `test-${faker.word.noun()}`;
+
   await test.step('When: El usuario hace clic en "Pages"', async () => {
     await page.getByRole('link', { name: 'Pages' }).click();
   });
@@ -48,18 +52,18 @@ test('creación de pagina publicada', async ({ page }) => {
 
   await test.step('And: El usuario crea el título de la nueva pagina', async () => {
     await page.getByPlaceholder('Page title').click();
-    await page.getByPlaceholder('Page title').fill('Nueva Pagina Test');
+    await page.getByPlaceholder('Page title').fill(newPageTitle);
+    await page.locator('button').filter({ hasText: '.close-stroke_svg__a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-l' }).click();
   });
 
   await test.step('And: El usuario publica la nueva pagina', async () => {
-    await page.locator('.koenig-editor__editor').click();
+    await page.getByRole('button', { name: 'Publish' }).click();
+    await page.getByText('Set it live now').click();
+    await page.getByRole('button', { name: 'Publish', exact: true }).click();
   });
-
-  await test.step('And: El usuario regresa la lista de paginas creadas', async () => {
-    await page.getByRole('link', { name: 'Pages' }).click();
-  });
-
-  await test.step('Then: La nueva pagina se muestra en la lista de paginas creadas', async () => {
-    expect(await page.title()).toBe('Nueva Pagina Test - grupo 25');
+  
+  await test.step('Then: El usuario puede abrir la nueva pagina publicada', async () => {
+    await page.goto(`/${newPageTitle.toLowerCase().replace(' ', '-')}`);
+    expect(await page.getByRole('heading', { name: newPageTitle }).innerText()).toBe(newPageTitle);
   });
 });
