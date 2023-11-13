bbec6155becadf882886934a96ac6de49c93135d
a-rodriguez-c <ja.rodriguezc12@uniandes.edu.co>
2023-11-12 20:15:44 -0500

    test: members

--- playwright/tests/members.spec.ts
+++ playwright/tests/members.spec.ts
@@ -13,42 +13,41 @@ test.describe('members', () => {
     const newMemberName = `test-${faker.word.noun()}`;
     const newMemberEmail = `test-${faker.internet.email()}`;
     const newMemberNote = `test-${faker.lorem.sentence()}`;
-
-    test.step('When: El usuario se dirige a la sección members', async () => {
-        await page.getByRole('link', { name: 'Members 0' }).click();
-        // await page.goto('/ghost/#/members')
-    });
     
-    test.step('And: El usuario hace clic en "New member"', async () => {
+    await test.step('When: El usuario se dirige a la sección members', async () => {
+        await page.locator('button').filter({ hasText: '.close-stroke_svg__a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-l' }).click();
+        await page.goto('/ghost/#/members');
+    });
+
+    await test.step('And: El usuario hace clic en "New member"', async () => {
         await page.getByRole('link', { name: 'New member' }).click();
     });
 
-    test.step('And: El usuario crea el nombre del nuevo miembro', async () => {
-        await page.getByLabel('Name').click();
+    await test.step('And: El usuario crea el nuevo miembro', async () => {
+        await page.getByLabel('Name').dblclick();
         await page.getByLabel('Name').fill(newMemberName);
-    });
-
-    test.step('And: El usuario crea el correo del nuevo miembro', async () => {
-        await page.getByLabel('Email').click();
+  
+        await page.getByLabel('Email').dblclick();
         await page.getByLabel('Email').fill(newMemberEmail);
-    });
-    
-    test.step('And: El usuario crea la nota del nuevo miembro', async () => {
-        await page.getByLabel('Note (not visible to member)').click();
+
+        await page.getByLabel('Note (not visible to member)').dblclick();
         await page.getByLabel('Note (not visible to member)').fill(newMemberNote);
     });
     
-    test.step('And: El usuario guarda el nuevo miembro', async () => {
+    await test.step('And: El usuario guarda el nuevo miembro', async () => {
         await page.getByRole('button', { name: 'Save' }).click();
+        await page.waitForTimeout(2000);
     });
 
-    test.step('Then: El nuevo miembro se muestra en la lista de miembros creados', async () => {
-        await page.goto('/ghost/#/members')
+    await test.step('Then: El nuevo miembro se muestra en la lista de miembros creados', async () => {
+        await page.goto('/ghost/#/members');
+        await page.waitForTimeout(1000);
         await page.getByRole('link', { name: `${newMemberName} ${newMemberEmail}` }).click();
         
         expect(await page.getByRole('heading', { name: newMemberName, exact: true }).innerText()).toBe(newMemberName);
         expect(await page.getByRole('link', { name: newMemberEmail }).innerText()).toBe(newMemberEmail);    
     });
+
   });
 
 });
