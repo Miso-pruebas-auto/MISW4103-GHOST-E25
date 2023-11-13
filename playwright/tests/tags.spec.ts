e95a4a6a46258414bf11297ac05d9aa605f4486f
camilo lesmes <c.lesmesl@uniandes.edu.co>
2023-11-12 20:58:14 -0500

    tags finalizado

--- playwright/tests/tags.spec.ts
+++ playwright/tests/tags.spec.ts
@@ -1,4 +1,4 @@
-import { Page, test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { loginSessionAdmin } from '../utils/login_sesion_admin';
 import { faker } from '@faker-js/faker';
 import { rgbToHex } from '../utils/rgb_to_hex';
@@ -54,11 +54,8 @@ test.describe('tags', () => {
     await test.step('And: Espera un segundo', async () => {
       await page.waitForTimeout(1000);
     });
-    // await test.step('And: El usuario se dirige al tag creado', async () => {
-    //   await page.goto(`/ghost/#/tags/${name_tag}/`);
-    // });
 
-    await test.step('Then: Se verifica ingresando a la URL del nuevo tag que tengo el nombre y color correctos', async () => {
+    await test.step('Then: Se verifica el nuevo tag contenga el nombre y color correctos', async () => {
       
     const url = page.url();
     expect(url.split('/tags/')[1]).toBe(name_tag);
@@ -71,10 +68,152 @@ test.describe('tags', () => {
     });
 
     const hexColor = rgbToHex(backgroundColor);
-    // Imprime el valor del estilo en la consola.
+
     console.log(hexColor);
     expect(hexColor).toBe(`#${color_tag}`);
 
     });
   });
+
+  test('Creación de tag sin color', async ({ page }) => {
+    const name_tag = faker.word.noun();
+    await test.step('When: El usuario hace clic en "Tags"', async () => {
+        await page.getByRole('link', { name: 'Tags' }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: hace clic en el botón de "New tag"', async () => {
+        await page.getByRole('link', { name: 'New tag', exact: true }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: Hace clic en el campo de tag y lo llena', async () => {
+      await page.getByLabel('Name').fill(name_tag);
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: hace clic en "Save"', async () => {
+      await page.getByRole('button', { name: 'Save' }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('Then: Se verifica que el nuevo tag solo tenga el nombre sin color', async () => {
+  
+    const url = page.url();
+    expect(url.split('/tags/')[1]).toBe(name_tag);
+
+    });
+  });
+
+  test('Creación de tag sin titulo', async ({ page }) => {
+    const name_tag = faker.word.noun();
+    await test.step('When: El usuario hace clic en "Tags"', async () => {
+        await page.getByRole('link', { name: 'Tags' }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: hace clic en el botón de "New tag"', async () => {
+        await page.getByRole('link', { name: 'New tag', exact: true }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: hace clic en "Save"', async () => {
+      await page.getByRole('button', { name: 'Save' }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('Then: Se valida el mensaje de tag sin título', async () => {
+    const error_title = "You must specify a name for the tag"
+    const validation_title =  await page.getByText('You must specify a name for the tag.').innerHTML();
+
+    expect(validation_title).toContain(error_title);
+
+    });
+  });
+
+  test('Creación de tag con título y color forzando dos veces el botón crear', async ({ page }) => {
+    const name_tag = faker.word.noun();
+    const color_tag = '666666';
+    await test.step('When: El usuario hace clic en "Tags"', async () => {
+        await page.getByRole('link', { name: 'Tags' }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: hace clic en el botón de "New tag"', async () => {
+        await page.getByRole('link', { name: 'New tag', exact: true }).click();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: Hace clic en el campo de tag y lo llena', async () => {
+      await page.getByLabel('Name').fill(name_tag);
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: Hace clic en el campo de color y lo llena', async () => {
+      await page.getByPlaceholder('15171A').fill(color_tag);
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('And: hace clic dos veces rápidamente en el "Save"', async () => {
+      const saveButton = await page.getByRole('button', { name: 'Save' });
+      await saveButton.dblclick();
+    });
+
+    await test.step('And: Espera un segundo', async () => {
+      await page.waitForTimeout(1000);
+    });
+
+    await test.step('Then: Se verifica que al dar doble clic en el botón de crear el nuevo tag contenga el nombre y color correctos', async () => {
+      
+    const url = page.url();
+    expect(url.split('/tags/')[1]).toBe(name_tag);
+    // Selecciona el input.
+    const input = await page.locator('.color-box-container');
+
+    // Obtiene el valor del estilo `background-color`.
+    const backgroundColor = await input.evaluate((input) => {
+      return window.getComputedStyle(input).getPropertyValue('background-color');
+    });
+
+    const hexColor = rgbToHex(backgroundColor);
+
+    console.log(hexColor);
+    expect(hexColor).toBe(`#${color_tag}`);
+
+    });
+  });
+  
 });
