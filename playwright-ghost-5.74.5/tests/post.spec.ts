d2eea784633cae7e4a5c5c59e28b96ca1fbdcf98
Mayistikar <andersonrodriguezce@gmail.com>
2023-12-02 12:10:16 -0500

    wip: ajustando Error creando post con excerpt de 5000 palabras random

--- playwright-ghost-5.74.5/tests/post.spec.ts
+++ playwright-ghost-5.74.5/tests/post.spec.ts
@@ -1205,7 +1205,8 @@ test.describe('Posts - Random data', () => {
 
   });
 
-  test('Crear un post con excerpt de 5000 palabras random', async ({ page }) => {
+  
+  test('Error creando post con excerpt de 5000 palabras random', async ({ page }) => {
     const titulo_post = faker.word.noun();
     const contenido = faker.lorem.paragraph();
     const excerpt = faker.word.words(5000);
@@ -1215,39 +1216,39 @@ test.describe('Posts - Random data', () => {
       await page.waitForTimeout(2000);
 
       await page.getByRole('link', { name: 'New post' }).click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Hace clic en el campo de título del post', async () => {
       await page.getByPlaceholder('Post title').click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Llena el título del post', async () => {
       await page.getByPlaceholder('Post title').fill(titulo_post);
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Presiona Tab', async () => {
       await page.getByPlaceholder('Post title').press('Tab');
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Llena el contenido del post', async () => {
       await page.getByRole('paragraph').click;
       await page.getByRole('paragraph').fill(contenido);
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Selecciona el botón de configuración', async () => {
       await page.getByRole('button', { name: 'Settings' }).click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Selecciona el campo tag haciendo clic en el tag "News"', async () => {
       await page.click('#tag-input');
       await page.getByRole('option', { name: 'News' }).click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Llena el excerpt', async () => {
@@ -1258,25 +1259,29 @@ test.describe('Posts - Random data', () => {
     await test.step('And: Hace clic en "Publish"', async () => {
       await page.waitForTimeout
       await page.getByRole('button', { name: 'Publish' }).click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: hace clic en confirmación de publicación', async () => {
       await page.getByRole('button', { name: 'Publish', exact: true }).click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
     await test.step('And: Hace clic en el botón de publicar para confirmar', async () => {
       await page.locator('button:has-text("Publish")').click();
-      await screenshotPagePath(page, 'post', 'Validar_si_deja_crear_un_post_sin_Autor', paso++);
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
-    await test.step('Then: Se verifica que el Post con excerpt se a creado correctamente', async () => {
-      await page.waitForTimeout(2000);
+    await test.step('Then: Se verifica que se genera un mensaje indicando que el excerpt es demasiado largo', async () => {
+      expect(page.getByText('Validation failed: Excerpt cannot be longer than 300 characters. Close')).toBeTruthy();
 
-      const errorMsg = await page.getByText('Saving failed: Excerpt cannot be longer than 300 characters.').innerText();
-      expect(errorMsg).toBe('Saving failed: Excerpt cannot be longer than 300 characters.');
-      await screenshotPagePath(page, 'post', 'Crear_un_nuevo_post_con_solo_título_y_descripción', paso++);
+      const errMsg = await page.getByText('Validation failed: Excerpt cannot be longer than 300 characters. Close').innerText();
+      const excerptErrMsg = await page.getByText('Excerpt cannot be longer than 300 characters.', { exact: true }).innerText();
+
+      expect(errMsg).toBe('Validation failed: Excerpt cannot be longer than 300 characters.');
+      expect(excerptErrMsg).toBe('Excerpt cannot be longer than 300 characters.');
+
+      await screenshotPagePath(page, 'post', 'Error_creando_post_con_excerpt_de_5000_palabras_random', paso++);
     });
 
   });
